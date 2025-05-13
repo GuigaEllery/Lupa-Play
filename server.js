67af22c4aee1efcfd6b6bbaf360114c3671a49ca
GuigaEllery <ellery@terra.com.br>
2025-05-12 23:57:39 -0300

    server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ Use este formato:
         'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
       },
       body: JSON.stringify({
-        model: 'gpt-4o',
+        model: 'gpt-3.5-turbo',
         messages: messages
       })
     });
