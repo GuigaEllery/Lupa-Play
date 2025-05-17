5e8d32f036193869c07afa2096a9d769efc47465
GuigaEllery <ellery@terra.com.br>
2025-05-17 01:24:51 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.post('/ask', async (req, res) => {
         topP: 1,
         maxOutputTokens: 2048,
       },
-      safetySettings: [
+      tools: [{
+  safetySettings: [
         { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_NONE },
         { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_NONE },
         { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_NONE },
