d4b717168744bcf94f78c8124cc700d8403b754e
GuigaEllery <ellery@terra.com.br>
2025-05-17 01:46:26 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log("Servidor rodando em http://localhost:" + port);
 });
